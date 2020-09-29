ffc54c369948200dc08bae4b13f26ced1543bb20
Lucas LEFRANCQ <llefranc@e1r3p12.42.fr>
2020-09-29 15:00:00 +0200

    todo list

--- llefranc/test/main.c
+++ llefranc/test/main.c
@@ -8,37 +8,19 @@ int main(int argc, char **argv)
 (void)argv;
 	
   int in, out;
-  char *grep_args[] = {"grep", "Villanova", NULL};
+//   char *grep_args[] = {"grep", "Villanova", NULL};
 
   // open input and output files
 
   in = open("scores", O_RDONLY);
-  out = open("out", O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+  out = open("harry", O_WRONLY | O_TRUNC | O_CREAT);
 
-  // replace standard input with input file
-  int stdin_var;
-  int stdout_var;
-
-	stdin_var = dup(0);
-	stdout_var = dup(0);
-
-  dup2(in, 0);
-
-  // replace standard output with output file
-
-  dup2(out, 1);
 
   // close unused file descriptors
 
   close(in);
   close(out);
 
-  // execute grep
-
-  execvp("grep", grep_args);
-  
-  dup2(stdin_var, 0);
-  dup2(stdout_var, 1);
 
 
 	printf("salut\n");
